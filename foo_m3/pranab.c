b07fb000762c1e96c39638682c202fd7cf75c187
PRANAB NANDY <34576104+PranabNandy@users.noreply.github.com>
2025-08-14 00:01:34 +0530

    Update pranab.c

--- foo_m3/pranab.c
+++ foo_m3/pranab.c
@@ -6,6 +6,15 @@ volatile uint8_t current_task = 0; // 0 = foo, 1 = myStartFunction
 
 
 //__attribute__((section(".data")))   --> Need to verify
+/*
+    Here’s why you see that "Cannot find bounds of current function" and why GDB can’t show it:
+
+GDB, when you step into that address, sees:
+    “Hmm… the PC is inside a section that’s not executable, 
+     so I have no symbol boundaries or debug lines for it.”
+
+even though the CPU happily runs the bytes (ARM doesn’t care about section flags).
+*/
 void foo() {
     int a = INT_MAX;
     while (a) {
