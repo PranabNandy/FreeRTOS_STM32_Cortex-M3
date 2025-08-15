c5351ae25f4bfd3a40016c9394c4fb1aa88e0ad0
PRANAB NANDY <34576104+PranabNandy@users.noreply.github.com>
2025-08-15 12:02:06 +0530

    Update pranab.c

--- foo_m3/pranab.c
+++ foo_m3/pranab.c
@@ -27,11 +27,13 @@ void Reset_Handler2(void) {
     Here’s why you see that "Cannot find bounds of current function" and why GDB can’t show it:
 
 GDB, when you step into that address, sees:
-    “Hmm… the PC is inside a section that’s not executable, 
+    “Hmm… the PC is inside a section that’s not executable,
      so I have no symbol boundaries or debug lines for it.”
 
 even though the CPU happily runs the bytes (ARM doesn’t care about section flags).
 */
+
+__attribute__((section(".ramfunc")))
 void foo() {
     int a = INT_MAX;
     while (a) {
@@ -62,6 +64,7 @@ void SysTick_Handler5(void) {   // 0x000002a8
 /// @param
 __attribute__((section(".new_section1")))
 void run_scheduler(void) {    // 0x00000288
+     Reset_Handler2();
     while (1) {
         if (current_task == 0) {
             foo();
